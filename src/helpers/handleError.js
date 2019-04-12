a38305b53499f82d6a40313fc477985c116986ce
foobardrum <tobias@eggut.ch>
2019-04-12 16:05:53 +0200

    smoother error handling function

--- src/helpers/handleError.js
+++ src/helpers/handleError.js
@@ -1,6 +1,13 @@
 export const handleError = (response) => {
-    if (!response.ok) {
-        throw response;
-    }
-    return response;
-}
+    return response.json()
+        .then(json => {
+            if (response.ok) {
+                return json
+            } else {
+                return Promise.reject(Object.assign({}, json, {
+                    status: json.status,
+                    statusText: json.statusText
+                }))
+            }
+        })
+};
