228b42457df8b99b6d03661227a7d06575ac1689
foobardrum <tobias@eggut.ch>
2019-04-12 16:36:43 +0200

    styled User List

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { BaseContainer } from "../../helpers/layout";
+import { Heading1, Main, MainContainer} from "../../helpers/layout";
 import { getDomain } from "../../helpers/getDomain";
 import Player from "../../views/Player";
 import { Spinner } from "../../views/design/Spinner";
@@ -8,11 +8,6 @@ import { Button } from "../../views/design/Button";
 import { withRouter } from "react-router-dom";
 import {handleError} from "../../helpers/handleError";
 import {catchError} from "../../helpers/catchError";
-
-const Container = styled(BaseContainer)`
-  color: white;
-  text-align: center;
-`;
 
 const Users = styled.ul`
   list-style: none;
@@ -25,6 +20,10 @@ const PlayerContainer = styled.li`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+const CenteredDiv = styled.div`
+  text-align: center;
 `;
 
 class Game extends React.Component {
@@ -49,7 +48,6 @@ class Game extends React.Component {
       }),
     })
       .then(handleError)
-      .then(response => response.json())
       .then( users => {
         this.setState({ users });
       })
@@ -58,38 +56,39 @@ class Game extends React.Component {
 
   render() {
     return (
-      <Container>
-        <h2>Players overview</h2>
-        <p>Get all users from secure end point:</p>
-        {!this.state.users ? (
-          <Spinner />
-        ) : (
-          <div>
-            <Users>
-              {this.state.users.map(user => {
-                return (
-                  <PlayerContainer
-                      key={user.id}
-                      onClick={()=> {
-                        this.props.history.push("/users/" + user.id)
-                      }}
-                  >
-                    <Player user={user} />
-                  </PlayerContainer>
-                );
-              })}
-            </Users>
-            <Button
-              width="100%"
-              onClick={() => {
-                this.logout();
-              }}
-            >
-              Logout
-            </Button>
-          </div>
-        )}
-      </Container>
+      <MainContainer>
+        <Main>
+          <Heading1>Players overview</Heading1>
+          {!this.state.users ? (
+            <Spinner />
+          ) : (
+            <CenteredDiv>
+              <Users>
+                {this.state.users.map(user => {
+                  return (
+                    <PlayerContainer
+                        key={user.id}
+                        onClick={()=> {
+                          this.props.history.push("/users/" + user.id)
+                        }}
+                    >
+                      <Player user={user} />
+                    </PlayerContainer>
+                  );
+                })}
+              </Users>
+              <Button
+                width="50%"
+                onClick={() => {
+                  this.logout();
+                }}
+              >
+                Logout
+              </Button>
+            </CenteredDiv>
+          )}
+        </Main>
+      </MainContainer>
     );
   }
 }
