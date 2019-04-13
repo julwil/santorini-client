99b39200d67b9e18851dd2de137e3dac7c09f07f
foobardrum <tobias@eggut.ch>
2019-04-13 21:27:24 +0200

    G02-64 adjusted catchError to support new Error component

--- src/helpers/catchError.js
+++ src/helpers/catchError.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 
-export const catchError = (err) => {
-
-    err.json().then( errorMessage => {
-        alert(errorMessage.message)
-    })
+export const catchError = (err, that) => {
+    if(err.status === 401){
+        localStorage.removeItem('token');
+        window.location = '/login';
+    }
+    console.log(err);
+    that.setState({error : err.message});
 };
