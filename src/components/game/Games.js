4a6453dca1da8537c0babcf6f985f50167ec7435
foobardrum <tobias@eggut.ch>
2019-04-25 18:56:45 +0200

    G02-85 changed board representation totally, added highlighted fields for move and build

--- src/components/game/Games.js
+++ src/components/game/Games.js
@@ -10,32 +10,30 @@ import {COLOR_5} from "../../helpers/layout";
 import {BoardField} from "./BoardField";
 
 const GameWrapper = styled.div`
-  height: calc(100% - 20px);
   overflow: hidden;
 `;
 const MainGame = styled.div`
+  overflow: hidden;
   display: flex;
   flex-wrap: nowrap;
-  height: calc(100% - 100px);
 `;
 const PlayerSidebar = styled.div`
-  width: 250px;
+  flex-grow: 1;
   margin-right: 20px;
   background-color: ${COLOR_5};
 `;
 const GameBoard = styled.div`
-  flex-grow: 1;
   background-color: ${COLOR_5};
+  height: calc(130px * 5);
 `;
 
 const BoardRow = styled.div`
   overflow: hidden;
 `;
 
-const OpponentSidebar = styled.div`
-  width: 250px;
+const OpponentSidebar = styled(PlayerSidebar)`
+  margin-right: 0;
   margin-left: 20px;
-  background-color: ${COLOR_5};
 `;
 
 class Games extends React.Component {
@@ -44,23 +42,79 @@ class Games extends React.Component {
         super();
         this.state = {
             gameId: null,
-            players:{
-                1:{id:1,user:1,active:false},
-                2:{id:2,user:1,active:false},
-                3:{id:3,user:2,active:true},
-                4:{id:4,user:2,active:false},
-            },
-            board: [
-                [{player:null,building:null},{player:null,building:null},{player:null,building:1},{player:4,building:null},{player:null,building:null},],
-                [{player:null,building:null},{player:1,building:null},{player:null,building:null},{player:null,building:null},{player:null,building:null},],
-                [{player:null,building:null},{player:null,building:null},{player:null,building:null},{player:null,building:null},{player:3,building:null},],
-                [{player:null,building:2},{player:null,building:null},{player:null,building:null},{player:null,building:2},{player:null,building:null},],
-                [{player:null,building:null},{player:null,building:null},{player:2,building:1},{player:null,building:3},{player:null,building:3},],
+            players:[
+                {id:1,user:1,active:false,x:0,y:0,possibleMoves:[],possibleBuilds:[]},
+                {id:2,user:1,active:false,x:3,y:0,possibleMoves:[],possibleBuilds:[]},
+                {id:3,user:2,active:true,x:1,y:3,possibleMoves:[{x:0,y:3},{x:2,y:3},{x:1,y:2},{x:1,y:4}],possibleBuilds:[]},
+                {id:4,user:2,active:false,x:3,y:2,possibleMoves:[],possibleBuilds:[]},
+            ],
+            buildings:[
+                {x:0,y:0,level:1},
+                {x:2,y:1,level:3},
+                {x:2,y:0,level:3},
+                {x:1,y:1,level:3},
+                {x:4,y:3,level:2},
+                {x:4,y:2,level:0},
+                {x:3,y:2,level:0},
             ],
             //game: this.props.location.state.game,
             error: []
         };
     }
+
+    getBuilding = (x,y) => {
+        let filteredBuildings = this.state.buildings.filter((building)=>{return building.x === x && building.y === y});
+        if(filteredBuildings.length > 0) return filteredBuildings[0];
+        return null;
+    };
+    getPlayer = (x,y) => {
+        let filteredPlayers =  this.state.players.filter((player) => {
+            return player.x === x && player.y === y;
+        });
+        if(filteredPlayers.length > 0) return filteredPlayers[0];
+        return null;
+    };
+    getActivePlayer = () => {
+        let filteredPlayers =  this.state.players.filter((player) => {
+            return player.active;
+        });
+        if(filteredPlayers.length > 0) return filteredPlayers[0];
+        return null;
+    };
+    isTargetForMove = (x,y) => {
+        let player = this.getActivePlayer();
+        if(player != null && player.hasOwnProperty('possibleMoves')){
+            let filteredMoves = player.possibleMoves.filter((move) => {return move.x === x && move.y === y});
+            console.log(filteredMoves,player);
+            return filteredMoves.length > 0;
+        }
+        return false;
+    };
+    isTargetForBuild = (x,y) => {
+        let player = this.getActivePlayer();
+        if(player != null && player.hasOwnProperty('possibleBuilds')){
+            let filteredBuilds = player.possibleBuilds.filter((build) => {return build.x === x && build.y === y});
+            return filteredBuilds.length > 0;
+        }
+        return false;
+    };
+    createBoard = () => {
+        let board = [];
+
+        for (let y = 0; y < 5; y++) {
+            let row = [];
+            for (let x = 0; x < 5; x++) {
+                row.push(<BoardField key={x}
+                                     building={this.getBuilding(x,y)}
+                                     player={this.getPlayer(x,y)}
+                                     targetForMove={this.isTargetForMove(x,y)}
+                                     targetForBuild={this.isTargetForBuild(x,y)}
+                />);
+            }
+            board.push(<BoardRow key={y}>{row}</BoardRow>);
+        }
+        return board;
+    };
 
     componentDidMount() {
         this.setState({gameId: this.props.match.params.gamesId});
@@ -93,13 +147,7 @@ class Games extends React.Component {
                 <MainGame>
                     <PlayerSidebar />
                     <GameBoard>
-                        {this.state.board.map( (row,i) => {
-                             return <BoardRow key={i}>
-                                { row.map((field,j) =>{
-                                    return <BoardField key={j} building={field.building} player={this.state.players[field.player]}/>
-                                })}
-                             </BoardRow>
-                        })}
+                        {this.createBoard()}
                     </GameBoard>
                     <OpponentSidebar />
                 </MainGame>
